718e38b4f6f7933e46c9c64c20f56e96c6a3bd2e
saeraphas <saeraphas@users.noreply.github.com>
2021-01-21 13:25:43 -0500

    move remarks from header to readme.md

--- Get-QBRData.ps1
+++ Get-QBRData.ps1
@@ -1,35 +1,10 @@
 <#
 .SYNOPSIS
-	Pull housekeeping reports from a domain controller for periodic review. 
-.DESCRIPTION
-	This script produces reports intended for customer review as part of 
-	periodic housekeeping, license audits, true-ups, etc. 
-	
-	Reports are generated in both HTML and CSV format - most of the HTML 
-	reports print out nicely and work well for print>strikethrough>scanback.
-	CSV is for doing fancy stuff. 	
-	
-	This script depends on the ActiveDirectory module and is intended to run
-	from a domain controller, though any member server or workstation with the
-	RSAT-AD-PowerShell module installed will work. 
-	#Install-WindowsFeature RSAT-AD-PowerShell
-	
-	It has been tested on Windows Server 2008R2 with WMF 4 and higher though
-	other platforms may also work. I'm not a real developer. 
+	This script collects data from a Windows AD domain controller and builds a number of reports intended for customer review as part of periodic housekeeping, license audits, true-ups, etc.
 
-	When run with a local SYSTEM account, this script produces
-	- AD users report with last login date and last password date
-	- AD custom groups membership report
-	- inactive users report
-	- inactive computers report
-	- workstation OS end-of-life inventory
-	- domain admin group membership report
-		
-	When run with a domain admin, this script additionally produces
-	- server storage space report 
-	- service accounts report
-	- server interface nameserver report
-		
+.DESCRIPTION
+	This script depends on WMF 4 and the ActiveDirectory Powershell module,  and is intended to run from a domain controller, though any domain member server or workstation with the RSAT-AD-PowerShell module installed should work.
+	
 .EXAMPLE
 	.\Get-QBRData.ps1
 
@@ -38,30 +13,8 @@
 	License: 			This script is distributed under "THE BEER-WARE LICENSE" (Revision 42):
 						As long as you retain this notice you can do whatever you want with this stuff.
 						If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
-    Changelog:
-        2020-08-28		Added script header for first github version. 
-        2020-09-02		Added check for NT AUTHORITY\SYSTEM to prevent ugly errors on storage report. 
-		2020-09-02		Added check to remove old output ZIP before creating a new one. 
-		2020-09-14		Added services report, Expanded customization of HTML header
-		2020-09-16		Fixed services report, output works as intended now.
-		2020-10-13		Added passwordlastset to domain admins report. 
-		2020-10-13		Added usersaudit report by request of Nexigen CTO CH. 
-		2020-10-19 		Added EOL OS report, moved and renamed per-report variables for legibility.
-		2020-10-20 		Added LastLogonDate to EOL OS report, only collect enabled accounts in usersaudit.
-		2020-12-02 		The McRib update: more meat! (moved report building to a function that always generates HTML and CSV)
-						Also fixed Domain Admins report, output now correctly includes nested groups.
-		2020-12-10		Added check for empty report data to prevent console error and 0-byte output files
-		2021-01-19		Added nameserver report. 
-		
-	Planned reports for future versions: 
-		365 account assigned licenses
-		365 account MFA status
-		365 account onprem sync status
-		365 account litigation hold status
-
 #>
 
-#DQDQBRDC - Douglas' Quick and Dirty QBR Data Collector
 #Requires -Version 4.0
 #Requires -Module activedirectory
 
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # get-qbrdata
+formerly DQDQBRDC (Douglas' Quick and Dirty QBR Data Collector)
+
+### INTRODUCTION
+This script collects data from a Windows AD domain controller and builds a number of reports intended for customer review as part of periodic housekeeping, license audits, true-ups, etc.
+
+### PREREQUISITES:
+This script depends on WMF 4 and the ActiveDirectory Powershell module,  and is intended to run from a domain controller, though any domain member server or workstation with the RSAT-AD-PowerShell module installed should work.
+```
+Install-WindowsFeature RSAT-AD-PowerShell
+```
+
+I have personally tested this script on Windows Server 2008R2 with WMF 4 and higher though older platforms may also work. Your mileage may vary.
+
+### USAGE:
+
+```
+.\Get-QBRData.ps1
+```
+
+There are currently no parameters or switches, but the reports available are dependant on the user account that executes the script.
+
+When run with a local SYSTEM account, this script produces
+- AD users report with last login date and last password date
+- AD custom groups membership report
+- inactive users report
+- inactive computers report
+- workstation OS end-of-life inventory
+- domain admin group membership report
+
+When run with a domain admin account, this script additionally produces
+- server storage space report 
+- service accounts report
+- server interface nameserver report
+
+Reports are generated in both HTML and CSV format.
+- HTML reports are generally suitable for review by non-technical customer contacts and can be used in a print>strikethrough>scanback flow.
+- CSV reports are generally suitable for review by customer contacts who have some familiarity with Excel, and can be used for automation if you want.
+
+### LICENSE:
+This script is distributed under "THE BEER-WARE LICENSE" (Revision 42):
+As long as you retain this notice you can do whatever you want with this stuff.
+If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
+
+### CONTACT:
+Douglas Hammond (douglas@douglashammond.com)
+
+### CHANGELOG:
+| Date | Note |
+| --- | --- |
+| 2020-08-28 | Added script header for first github version.|
+| 2020-09-02 | 	Added check for NT AUTHORITY\SYSTEM to prevent ugly errors on storage report.|
+| 2020-09-02 | Added check to remove old output ZIP before creating a new one. |
+| 2020-09-14 | Added services report, Expanded customization of HTML header |
+| 2020-09-16 | Fixed services report, output works as intended now. |
+| 2020-10-13 | Added passwordlastset to domain admins report. |
+| 2020-10-13 | Added usersaudit report by request. |
+| 2020-10-19 | Added EOL OS report, moved and renamed per-report variables for legibility. |
+| 2020-10-20 | Added LastLogonDate to EOL OS report, only collect enabled accounts in usersaudit. |
+| 2020-12-02 | The McRib update: more meat! (moved report building to a function that always generates HTML and CSV). Also fixed Domain Admins report, output now correctly includes nested groups. |
+| 2020-12-10 | Added check for empty report data to prevent console error and 0-byte output files. |
+| 2021-01-19 | Added nameserver report.|
+| 2021-01-21 | Moved changelog and some remarks from header to README.md |
+
+### ROADMAP:
+- 365 account assigned licenses
+- 365 account MFA status
+- 365 account onprem sync status
+- 365 account litigation hold status
