1c9f52a6e647b71fbd71f2e23d7bd47b180796bf
saeraphas <saeraphas@users.noreply.github.com>
2023-01-19 09:48:50 -0500

    Update Get-QBRData.ps1
    add link

--- Get-QBRData.ps1
+++ Get-QBRData.ps1
@@ -13,6 +13,9 @@
 	License: 			This script is distributed under "THE BEER-WARE LICENSE" (Revision 42):
 						As long as you retain this notice you can do whatever you want with this stuff.
 						If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
+
+.LINK
+	https://github.com/saeraphas/get-qbrdata
 #>
 
 #Requires -Version 4.0
