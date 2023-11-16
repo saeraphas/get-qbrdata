5d67be9dea33eb5c53366caab505ee60d4693f4c
saeraphas <saeraphas@users.noreply.github.com>
2023-11-16 10:55:55 -0500

    Update Get-QBRData.ps1

--- Get-QBRData.ps1
+++ Get-QBRData.ps1
@@ -3,7 +3,7 @@
 	This script collects data from a Windows AD domain controller and builds a number of reports intended for customer review as part of periodic housekeeping, license audits, true-ups, etc.
 
 .DESCRIPTION
-	This script depends on WMF 4 and the ActiveDirectory Powershell module,  and is intended to run from a domain controller, though any domain member server or workstation with the RSAT-AD-PowerShell module installed should work.
+	This script depends on WMF 5.1 and the ActiveDirectory Powershell module, and is intended to run from a domain controller, though any domain member server or workstation with the RSAT-AD-PowerShell module installed should work.
 	
 .EXAMPLE
 	.\Get-QBRData.ps1
